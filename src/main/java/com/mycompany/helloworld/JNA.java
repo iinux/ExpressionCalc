4303d25929f3468995a9238a25043b7b2409aa9d
iinux <iinux@qq.com>
2019-05-23 15:28:48 +0800

    jna

--- src/main/java/com/mycompany/helloworld/JNA.java
+++ src/main/java/com/mycompany/helloworld/JNA.java
@@ -3,6 +3,9 @@ package com.mycompany.helloworld;
 import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.Platform;
+import com.sun.jna.WString;
+
+// https://www.cnblogs.com/lanxuezaipiao/p/3635556.html
 
 public class JNA {
     // This is the standard, stable way of mapping, which supports extensive
@@ -16,10 +19,29 @@ public class JNA {
         void printf(String format, Object... args);
     }
 
+    public interface Kernel32 extends Library {
+        Kernel32 INSTANCE = (Kernel32) Native.loadLibrary("kernel32", Kernel32.class);
+        public boolean Beep(int FREQUENCY, int DURATION);
+        public void Sleep(int DURATION);
+    }
+
+    public interface User32 extends Library {
+        User32 INSTANCE = (User32) Native.loadLibrary("user32", User32.class);
+        public boolean LockWorkStation();
+        public int MessageBoxA(int something, String text, String caption, int flags);
+        public int MessageBoxW(int something, WString text, WString caption, int flags);
+    }
+
     public static void main(String[] args) {
         CLibrary.INSTANCE.printf("Hello, World\n");
         for (int i = 0; i < args.length; i++) {
             CLibrary.INSTANCE.printf("Argument %d: %s\n", i, args[i]);
         }
+
+        // Kernel32.INSTANCE.Beep(698, 1500);
+        // Kernel32.INSTANCE.Sleep(500);
+        // User32.INSTANCE.LockWorkStation();
+        // User32.INSTANCE.MessageBoxA(0,"MessageBox success!!!","Attention",0);
+        User32.INSTANCE.MessageBoxW(0, new WString("MessageBox success!!!"), new WString("Attention"), 0);
     }
 }
