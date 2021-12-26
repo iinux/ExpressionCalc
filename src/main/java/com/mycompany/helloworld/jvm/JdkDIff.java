bbbf43ccf4e911cb000a4969b3534d1eeb961c18
iinux <iinux@qq.com>
2021-12-26 18:31:19 +0800

    jdk diff

---
+++ src/main/java/com/mycompany/helloworld/jvm/JdkDIff.java
@@ -0,0 +1,15 @@
+package com.mycompany.helloworld.jvm;
+
+import java.net.URL;
+
+public class JdkDIff {
+    public static void main(String[] args) {
+        URL r = ClassLoader.getSystemResource("java/lang/Class.class");
+        System.out.println(r);
+        // jdk 8
+        // jar:file:/home/qzhang/Downloads/jdk8u312-b07/jre/lib/rt.jar!/java/lang/Class.class
+        // jdk 11
+        // jrt:/java.base/java/lang/Class.class
+        // refer https://www.cnblogs.com/tankaixiong/p/14959796.html
+    }
+}
