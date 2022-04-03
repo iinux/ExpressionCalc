d022fa2b4cd366fb8c07fcbe2b6c73413e979ffe
iinux <iinux@qq.com>
2022-04-03 21:52:08 +0800

    TestTrace

---
+++ src/main/java/com/mycompany/helloworld/StreamStudy.java
@@ -0,0 +1,17 @@
+package com.mycompany.helloworld;
+
+import org.junit.Test;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamStudy {
+    @Test
+    public void TestTrace() {
+        Stream.of("beijing","tianjin","shanghai","wuhan")
+                .map(String::length)
+                .filter(e->e>5)
+                .collect(Collectors.toList());
+        // StreamTrace
+    }
+}
