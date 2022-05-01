f6b998ff087cdcf8a4f58a3381a4bef0526853d6
iinux <iinux@qq.com>
2022-05-01 10:02:42 +0800

    10 basic

--- src/main/java/com/mycompany/helloworld/basic/BackspaceTest.java
+++ src/main/java/com/mycompany/helloworld/basic/BackspaceTest.java
@@ -2,7 +2,7 @@ package com.mycompany.helloworld.basic;
 
 public class BackspaceTest {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.print("hello");
         System.out.print("\b");
         System.out.println("hi");
     }
---
+++ src/main/java/com/mycompany/helloworld/basic/SystemProperty.java
@@ -0,0 +1,34 @@
+package com.mycompany.helloworld.basic;
+
+import sun.misc.Launcher;
+import sun.reflect.Reflection;
+
+import java.util.stream.Stream;
+
+public class SystemProperty {
+    public static void main(String[] args) {
+        printProperty("sun.boot.class.path");
+        printProperty("java.ext.dirs");
+        printProperty("java.class.path");
+        printProperty("java.system.class.loader");
+
+        System.getProperties().list(System.out);
+
+        System.out.println(ClassLoader.class.getClassLoader());
+        System.out.println(Launcher.class.getClassLoader());
+        System.out.println(Thread.class.getClassLoader());
+        System.out.println(Thread.currentThread().getContextClassLoader());
+
+        // System.out.println(Reflection.getCallerClass());
+    }
+
+    public static void printProperty(String name) {
+        System.out.println("name = " + name);
+        String property = System.getProperty(name);
+        if (property == null) {
+            System.out.println("is null");
+            return;
+        }
+        Stream.of(property.split(":")).forEach(System.out::println);
+    }
+}
