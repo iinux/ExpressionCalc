5b6775f65b1415a7ce363473610c335ed47ed2cc
iinux <iinux@qq.com>
2022-05-03 00:22:05 +0800

    mybatis

--- src/main/java/com/mycompany/helloworld/springboot/MybatisUtil.java
+++ src/main/java/com/mycompany/helloworld/springboot/MybatisUtil.java
@@ -22,43 +22,39 @@ public class MybatisUtil {
     private static final ThreadLocal<SqlSession> threadLocal = new ThreadLocal<>();
     private static SqlSessionFactory sqlSessionFactory;
 
-    public static void init() {
+    public void init() {
         try {
             Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
             sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+            // InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+            // sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         } catch (IOException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
     }
 
-    public static SqlSession getSqlSession() {
-        //从当前线程中获取SqlSession对象
+    @Test
+    public void getSqlSession() {
         SqlSession sqlSession = threadLocal.get();
-        //如果SqlSession对象为空
         if (sqlSession == null) {
-            //在SqlSessionFactory非空的情况下，获取SqlSession对象
+            if (sqlSessionFactory == null) {
+                init();
+            }
             sqlSession = sqlSessionFactory.openSession();
-            //将SqlSession对象与当前线程绑定在一起
             threadLocal.set(sqlSession);
         }
 
         Connection conn = sqlSession.getConnection();
         System.out.println(conn != null ? "连接成功" : "连接失败");
-
-        //返回SqlSession对象
-        return sqlSession;
     }
 
     /**
      * 关闭SqlSession与当前线程分开
      */
     public static void closeSqlSession() {
-        //从当前线程中获取SqlSession对象
         SqlSession sqlSession = threadLocal.get();
-        //如果SqlSession对象非空
         if (sqlSession != null) {
-            //关闭SqlSession对象
             sqlSession.close();
             //分开当前线程与SqlSession对象的关系，目的是让GC尽早回收
             threadLocal.remove();
@@ -76,14 +72,5 @@ public class MybatisUtil {
         DefaultShellCallback callback = new DefaultShellCallback(true);
         MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
         myBatisGenerator.generate(null);
-    }
-
-    public void db() throws IOException {
-        SqlSession sqlSession = null;
-        InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-
-        // 查询数据库内容
-        sqlSession = sqlSessionFactory.openSession();
     }
 }
