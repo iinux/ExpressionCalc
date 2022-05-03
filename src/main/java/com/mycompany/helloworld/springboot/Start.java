673488a218f2e24b4c47362d300dd871650ff1b0
iinux <iinux@qq.com>
2022-05-03 12:42:08 +0800

    example ok

--- src/main/java/com/mycompany/helloworld/springboot/Start.java
+++ src/main/java/com/mycompany/helloworld/springboot/Start.java
@@ -2,11 +2,18 @@ package com.mycompany.helloworld.springboot;
 
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.io.IOException;
+import java.io.Reader;
 
 @SpringBootApplication(
         scanBasePackages = "com.mycompany.helloworld.springboot")
@@ -21,5 +28,11 @@ public class Start {
         JSONObject returnObject = new JSONObject();
         returnObject.put("a", "b");
         log.info("started, obj={}", returnObject);
+    }
+
+    // @Bean
+    public SqlSessionFactory sqlSessionFactory() throws IOException {
+        Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+        return new SqlSessionFactoryBuilder().build(reader);
     }
 }
