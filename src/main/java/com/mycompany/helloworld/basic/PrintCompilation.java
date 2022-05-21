dfd9f6ffdd64e7c38cb2668a87520b7198e11f09
iinux <iinux@qq.com>
2022-05-21 20:51:33 +0800

    print comilation

---
+++ src/main/java/com/mycompany/helloworld/basic/PrintCompilation.java
@@ -0,0 +1,9 @@
+package com.mycompany.helloworld.basic;
+
+// vm option -XX:+PrintCompilation -verbose:class
+
+public class PrintCompilation {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
