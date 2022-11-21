974506e8cc888ba05c170a0f820d979802bf0900
iinux <iinux@qq.com>
2022-11-21 22:17:07 +0800

    ognl

---
+++ src/main/java/com/mycompany/helloworld/ognl/ADemo.java
@@ -0,0 +1,13 @@
+package com.mycompany.helloworld.ognl;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ADemo {
+    private String name;
+    private Integer age;
+}
