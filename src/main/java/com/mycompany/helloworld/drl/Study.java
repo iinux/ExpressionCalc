7a39a4d68d5c8a46f98d994763f8036170591743
iinux <iinux@qq.com>
2023-05-04 18:17:41 +0800

    drl

---
+++ src/main/java/com/mycompany/helloworld/drl/Study.java
@@ -0,0 +1,22 @@
+package com.mycompany.helloworld.drl;
+
+import org.junit.Test;
+
+public class Study {
+    @Test
+    public void test() {
+        /*
+        KieContainer container = null;
+        KieSession statefulKieSession = null;
+        KieServices kieServices = KieServices.Factory.get();
+        container = kieServices.getKieClasspathContainer();
+        statefulKieSession = container.newKieSession("myAgeSession");
+        User user = new User("duval yang", 12);
+        statefulKieSession.insert(user);
+        statefulKieSession.fireAllRules();
+        statefulKieSession.dispose();
+
+         */
+
+    }
+}
---
+++ src/main/java/com/mycompany/helloworld/drl/User.java
@@ -0,0 +1,9 @@
+package com.mycompany.helloworld.drl;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class User {
+    public String name;
+    public int age;
+}
